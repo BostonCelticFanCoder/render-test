38ed4760de342b6860a072ebabeb93c06d0172ba
Maksim Yin <yinmaksim@gmail.com>
2024-01-21 09:46:45 -0800

    Backend Commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fortSize: 16
+    }
+    return (
+        <div style={footerStyle}>
+            <br />
+            <em>Note app; Maksim Yin, Los Angeles California</em>
+        </div>
+    )
+}
+
+export default Footer
